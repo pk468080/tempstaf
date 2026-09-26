5a7e6134e976c527f5d46c15d1785c6d56cb093e
Prince Rajput <princerajput@Princes-MacBook-Air.local>
2026-09-26 20:19:03 +0530

    revert: restore TempStaff customer app

--- customer-app/App.tsx
+++ customer-app/App.tsx
@@ -1,9 +1,5 @@
-import { View, Text } from "react-native";
+import RootNavigator from './navigation/RootNavigator'
 
 export default function App() {
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Hello Expo</Text>
-    </View>
-  );
+  return <RootNavigator />
 }
